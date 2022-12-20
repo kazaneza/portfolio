2931b44500580871f593cd2f824ad6a54af18e0c
kazaneza <kazanezagentil@gmail.com>
2022-12-20 13:22:05 +0100

    > Add no scroll >Dropdown menu cover the whole screen

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,31 @@
-const menu = document.querySelector('.header__menu');
-const openMenuBtn = document.querySelector('.mobile-nav');
-const closeMenuBtn = document.querySelector('.header__menu-btn');
-const menuOptions = document.querySelectorAll('.header__menu-link');
+const body = document.querySelector(".body");
+const menu = document.querySelector(".header__menu");
+const openMenuBtn = document.querySelector(".mobile-nav");
+const closeMenuBtn = document.querySelector(".header__menu-btn");
+const menuOptions = document.querySelectorAll(".header__menu-link");
 
 const openMenu = () => {
-  menu.classList.remove('close');
-  menu.classList.add('open');
+  menu.classList.remove("close");
+  menu.classList.add("open");
+  body.classList.add("no-scroll");
 };
 
 const closeMenu = () => {
-  menu.classList.remove('open');
-  menu.classList.add('close');
+  menu.classList.remove("open");
+  menu.classList.add("close");
+  body.classList.remove("no-scroll");
 };
 
-openMenuBtn.addEventListener('click', () => {
+openMenuBtn.addEventListener("click", () => {
   openMenu();
 });
 
-closeMenuBtn.addEventListener('click', () => {
+closeMenuBtn.addEventListener("click", () => {
   closeMenu();
 });
 
 menuOptions.forEach((option) => {
-  option.addEventListener('click', () => {
+  option.addEventListener("click", () => {
     closeMenu();
   });
 });
